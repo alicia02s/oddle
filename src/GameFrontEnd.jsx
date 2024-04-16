6529d8976f532d41493f976dd72423ecf142fab0
LJ Labata <louiejoshualabata@gmail.com>
2024-04-15 19:52:47 -0700

    etc?

---
+++ src/GameFrontEnd.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './LandingPage.css'
+
+import oddleLogo from './../public/oddle.svg'
+
+function GameFrontEnd() {
+  return (
+    <div className='GameFrontEndDiv'>
+      <div className='NavBar'>
+
+      </div>
+    </div>
+  )
+}
+
+export default GameFrontEnd
