3624a29c7ac32dbb7e2d1c232ea74b1ffc82a7f8
rkumar06 <riyakumar06@gmail.com>
2024-04-15 19:36:55 -0700

    bug fixed oddle added, words working

--- src/Game.jsx
+++ src/Game.jsx
@@ -4,6 +4,7 @@ function Game() {
     const [numSeedWords, setNumSeedWords] = useState(3);
     const [seedWords, setSeedWords] = useState([]);
     const [allWords, setAllWords] = useState([]);
+    const [oddle, setOddle] = useState("");
 
     async function fetchSeedWord() {
         const response = await fetch(`https://random-word-api.vercel.app/api?words=1`);
@@ -20,7 +21,7 @@ function Game() {
         });
         const data = await response.json();
         if (data.synonyms && data.synonyms.length > 0) {
-            return data.synonyms.slice(0, 2);  // Return only the first two synonyms
+            return [data.synonyms.slice(0, 2), word];  // Return only the first two synonyms
         } else {
             // If no synonyms, fetch a new seed word and try again
             const newSeed = await fetchSeedWord();
@@ -33,10 +34,16 @@ function Game() {
             const seeds = await fetchSeedWords();  // Fetch initial seed words
             setAllWords([]);  // Clear allWords before populating
 
+            const oddleWord = await fetchSeedWord();
+            setOddle(oddleWord);
+            setAllWords([oddleWord])
+
             for (let i = 0; i < seeds.length; i++) {
                 const seed = seeds[i];
-                const alikeWords = await fetchAlikeWords(seed);  // Fetch alike words for each seed
-                setAllWords(prevWords => [...prevWords, seed, ...alikeWords]);  // Add both seed and its alike words
+                const result = await fetchAlikeWords(seed);  // Fetch alike words for each seed
+                const alikeWords = result[0]
+                const newSeed = result[1]
+                setAllWords(prevWords => [...prevWords, newSeed, ...alikeWords]);  // Add both seed and its alike words
             }
         } catch (error) {
             console.error('Failed to fetch words:', error);
@@ -58,8 +65,8 @@ function Game() {
         <div>
             <h1 onClick={fetchSeedWords}>Get Seed Words</h1>
             <button onClick={getAllWords}>Get All Words</button>
+            <h2>Oddle: {oddle}</h2>
             <h2>Number of Seed Words: {seedWords.length}</h2>
-            <h3>Seed Words: {seedWords.join(', ')}</h3>
             <h3>All Words:</h3>
             <ul>
                 {allWords.map((word, index) => (
