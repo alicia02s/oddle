9e2e9406f97b392800e992cda49db4a1045438d8
Hillary Khuu <hillarykhuu@berkeley.edu>
2024-04-21 22:57:30 -0700

    added emoji cursor

--- oddle_frontend/src/pages/LandingPage/LandingPage.jsx
+++ oddle_frontend/src/pages/LandingPage/LandingPage.jsx
@@ -7,21 +7,25 @@ import Button from '../../components/Button'
 function LandingPage() {
   return (
     <div className='LandingPageDiv'>
-      <img src={oddleLogo} className="logo react" alt="React logo"/>
+      {/* these link tags allow us to use external fonts from Google API */}
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+      <link href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap" rel="stylesheet"></link>
+      <img src={oddleLogo} className="logo-react button-emoji" alt="React logo"/>
       <div className='wavy'>
         <span style={{ '--i': '1'}}>O</span>
-        <span style={{ '--i': '2'}}>D</span>
-        <span style={{ '--i': '3'}}>D</span>
-        <span style={{ '--i': '4'}}>L</span>
-        <span style={{ '--i': '5'}}>E</span>
-        <span style={{ '--i': '6'}}>!</span>
+        <span style={{ '--i': '2'}}>d</span>
+        <span style={{ '--i': '3'}}>d</span>
+        <span style={{ '--i': '4'}}>l</span>
+        <span style={{ '--i': '5'}}>e</span>
+        {/* <span style={{ '--i': '6'}}>!</span> */}
       </div>
-      <h2>Given X words, select the word out.</h2>
+      <h2>Given X words, select the odd word out.</h2>
       <Button textInButton="Play Oddle" color="normal" size="normal" linkTo="/Game" />
 
       <h4>April 11, 2024</h4>
       <h4>No. 0</h4>
-      <h4>Edited by Rahl</h4>
+      <h4 className="button-emoji">Edited by Rahl</h4>
     </div>
   )
 }
